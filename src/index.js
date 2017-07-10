94c8d5f328ff3e8c8a929bdcc1ee4d5ab514f262
finishearly <derek.irving@finishearly.com>
2017-07-10 16:59:24 +0100

    Added on first commit

---
+++ src/index.js
@@ -0,0 +1,91 @@
+const ConcatSource = require("webpack-sources").ConcatSource;
+const path = require("path");
+const {
+    readFileSync,
+    writeFileSync,
+    mkdirSync
+} = require("fs");
+const gettextToI18Next = require("i18next-conv").gettextToI18next;
+
+function I18nPlugin(options) {
+    this.options = options || {};
+}
+
+function save(target) {
+    return result => {
+        writeFileSync(target, result);
+    };
+}
+
+const mkdir = function (dirPath) {
+    try {
+        mkdirSync(dirPath);
+    } catch (err) {
+        if (err.code !== "EEXIST") throw err;
+    }
+};
+
+I18nPlugin.prototype.apply = function (compiler) {
+    var self = this;
+    if (self.options.locale[1] !== null) {
+        const poPath = path.join(self.options.localesPath, self.options.locale[1]);
+        const locale = self.options.locale[0];
+
+        mkdir(path.resolve(path.join(self.options.srcPath, "/tmp/")));
+
+        gettextToI18Next(locale, readFileSync(poPath), {})
+            .then(save(path.join(self.options.srcPath, `/tmp/${locale}.json`)));
+    }
+
+    compiler.plugin("done",
+        function () {
+            console.log(`i18n translated ${self.options.locale}`);
+        });
+
+    var regex = typeof (self.options.regex) === "undefined" ?
+        /\[\[\[(.+?)(?:\|\|\|(.+?))*(?:\/\/\/(.+?))?\]\]\]/g :
+        self.options.regex;
+
+    compiler.plugin("compilation",
+        function (compilation) {
+
+            compilation.plugin("optimize-chunk-assets",
+                function (chunks, callback) {
+
+                    var locale;
+                    if (self.options.locale[1] !== null) {
+                        locale = require(path.join(self.options.srcPath, `/tmp/${self.options.locale[0]}.json`));
+                    }
+
+                    chunks.forEach(function (chunk) {
+                        chunk.files.forEach(function (file) {
+                            var source = compilation.assets[file].source();
+                            while ((m = regex.exec(source)) !== null) {
+
+                                if (m.index === regex.lastIndex) {
+                                    regex.lastIndex++;
+                                }
+
+                                if (self.options.locale[1] === null) {
+                                    source = source.replace(m[0], m[1]);
+                                } else {
+                                    const replacement = locale[m[1]];
+                                    if (typeof (replacement) === "undefined") {
+                                        compilation.warnings.push(
+                                            new Error(`Missing translation, '${m[1]}' : ${self.options.locale[0]}`));
+                                    } else {
+                                        source = source.replace(m[0], replacement);
+                                    }
+                                }
+                            }
+
+                            compilation.assets[file] =
+                                new ConcatSource(`/**i18n replaced ${self.options.locale[0]}**/`, "\n", source);
+                        });
+                    });
+                    callback();
+                });
+        });
+};
+
+module.exports = I18nPlugin;
