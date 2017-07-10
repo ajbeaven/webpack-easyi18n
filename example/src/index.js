e7e1b31d5822b73c8f5d905193a5549fb64c200a
finishearly <derek.irving@finishearly.com>
2017-07-10 17:01:21 +0100

    Added on first commit

---
+++ example/src/index.js
@@ -0,0 +1,5 @@
+document.write("[[[Login using]]]");
+document.write("[[[Log off]]]");
+document.write("[[[Page not found]]]");
+document.write("[[[Server Error]]]");
+document.write("[[[Forgot Translation]]]");
