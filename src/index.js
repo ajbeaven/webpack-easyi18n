6e58c76b9d86a9659caec209728ec2c9cb8428cc
Andrew Beaven <andrew@simplydating.com>
2021-04-14 11:57:14 +1200

    Make options.alwaysRemoveBrakets true by default, which matches how EasyI18n operates.

--- src/index.js
+++ src/index.js
@@ -7,9 +7,13 @@ const {
 } = require("fs");
 const gettextToI18Next = require("i18next-conv").gettextToI18next;
 
-function I18nPlugin(locale, options) {
+const defaultOptions = {
+    alwaysRemoveBrackets: true,
+};
+
+function I18nPlugin(locale, options = {}) {
     this.locale = locale;
-    this.options = options || {};
+    this.options = Object.assign({}, options, defaultOptions);
 }
 
 function save(target) {
